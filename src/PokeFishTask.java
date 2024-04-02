b610c9de485a27cad00c5764c58f542424882fca
root <parny_fan2003@doglover.com>
2024-04-02 02:18:17 +0100

    wow

--- src/PokeFishTask.java
+++ src/PokeFishTask.java
@@ -1,9 +1,10 @@
 import com.google.inject.Singleton;
 import org.rspeer.commons.logging.Log;
+import org.rspeer.commons.math.Distance;
 import org.rspeer.event.Subscribe;
 import org.rspeer.game.Game;
 import org.rspeer.game.adapter.scene.Npc;
-import org.rspeer.game.adapter.scene.PathingEntity;
+import org.rspeer.game.combat.Combat;
 import org.rspeer.game.event.ChatMessageEvent;
 import org.rspeer.game.query.results.SceneNodeQueryResults;
 import org.rspeer.game.scene.Npcs;
@@ -27,6 +28,8 @@ public class PokeFishTask extends Task {
     private final Map<Integer, Integer> pokedFish = new HashMap<Integer, Integer>();
     private final String POKE_FISH_MESSAGE  = "You prod at the shoal of fish";
 
+    private int target = -1;
+
     @Subscribe
     public void notify(ChatMessageEvent event) {
         if (event.getType() != ChatMessageEvent.Type.FILTERED) {
@@ -36,8 +39,6 @@ public class PokeFishTask extends Task {
         if (!event.getContents().contains(POKE_FISH_MESSAGE)) {
             return;
         }
-
-        Integer target = Players.self().getTargetIndex();
 
         if (target == -1) {
             Log.fine("Message appeared but we have no target");
@@ -65,11 +66,16 @@ public class PokeFishTask extends Task {
             return false;
         }
 
-        Npc nearest = fish.nearest();
-
-        if (nearest.distance(Players.self()) == 1) { // dodesnt appear as targget if 1 tile away ? idfk
-            pokedFish.put(nearest.getIndex(), Game.getTick());
+        Npc nearest = fish.nearest(Distance.MANHATTAN);
+        if (nearest == null) {
+            return false;
         }
+
+        if (Combat.getSpecialEnergy() == 100) {
+            Combat.toggleSpecial(true);
+        }
+
+        target = nearest.getIndex();
 
         nearest.interact("Chase");
         return true;
